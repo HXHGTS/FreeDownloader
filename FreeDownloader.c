32d8449298855d70415d2682f78be1cf0df51f9f
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-06-11 01:06:45 +0800

    Add files via upload

--- FreeDownloader.c
+++ FreeDownloader.c
@@ -1,12 +1,12 @@
 ﻿#include<stdio.h>
 #include<stdlib.h>
 #include<io.h>
-int downloadmode, magnet_mode,config_thread, config_media, anti_shutdown, Download_Task, IsCheckSum,Auto_ChangeUA;
+int downloadmode, magnet_mode,config_thread, config_media, anti_shutdown, Download_Task, IsCheckSum;
 char config_proxy[65], config_url[200], config_dir[35], config_cookie[40], cmd[1450], Downloader_Use[15];
 char reference[216], head[300], head_show[30];
 char location[200],split[7],torrent_loca[200],play_list[15];
 int mark,proxymode, download_result2,  shutdown, filecheck,use_list;
-FILE* * conf,*save,*power_ini,*space,*dic,*Media_conf;
+FILE* * conf,*save,*power_ini,*dic,*Media_conf;
 
 int CreateFolder() {
 	if (!access("Downloads", 0)) {
@@ -18,6 +18,7 @@ int CreateFolder() {
 		Media_conf = fopen("Media.conf", "w");
 		fprintf(Media_conf, "dir=Downloads\n");
 		fprintf(Media_conf, "continue=true\n");
+		fprintf(Media_conf, "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36\n");
 		fprintf(Media_conf, "max-concurrent-downloads=1\n");
 		fprintf(Media_conf, "max-connection-per-server=16\n");
 		fprintf(Media_conf, "min-split-size=2M\n");
@@ -38,7 +39,7 @@ int main() {
 p_3:printf("------------------------------------------------\n");
 	printf("---------------- FreeDownloader ----------------\n");
 	printf("------------------------------------------------\n");
-	printf("请选择下载功能：\n1.普通下载模式\n2.百度网盘下载\n3.视频下载模式\n4.高级下载模式\n5.导入下载模式\n6.磁力链下载模式（实验性）\n7.文件完整性测试\n8.Github上的软件帮助\n9.打开下载文件夹\n0.退出\n");
+	printf("请选择下载功能：\n1.普通下载模式\n2.百度网盘下载\n3.视频下载模式\n4.高级下载模式\n5.导入下载模式\n6.磁力链下载模式\n7.文件完整性测试\n8.Github上的软件帮助\n9.打开下载文件夹\n0.退出\n");
 	printf("------------------------------------------------\n");
 	printf("请输入：");
 	scanf("%d", &downloadmode);
@@ -115,26 +116,9 @@ p_2:download_result2 = downloadengine();
 			goto p_2;
 		}
 		else {
-			if (mark != 3 && Auto_ChangeUA != 0) {
-				sprintf(reference, "%s", "--referer=\"http://pan.baidu.com/disk/home\"");
-				mark = 3;
-				sprintf(head, "--header=\"User-Agent:%s\"", "netdisk;6.8.9.1;PC;PC-Windows;10.0.18363;WindowsBaiduYunGuanJia");//百度网盘
-				sprintf(head_show, "%s", "百度网盘（兼容模式）");
-				if (mark != 3)config_thread = 5;
-				else config_thread = 3;
-				sprintf(split, "1M");
 				system("cls");
 				goto p_2;
 			}
-			else {
-				if (mark != 3)config_thread = 5;
-				else config_thread = 3;
-				if (mark != 3)sprintf(split, "1M");
-				else sprintf(split, "1M");
-				system("cls");
-				goto p_2;
-			}
-		}
 	}
 }//下载工具主程序
 
@@ -163,21 +147,46 @@ int MagnetDownloader() {
 		dir();
 	}
 	threader();
-	printf("\n正在本地建立BT配置文件. . .\n");
-	if ((conf = fopen("bt.conf", "r")) == NULL) {
-		conf = fopen("bt.conf", "w");
-		fprintf(conf, "##bt-tracker=server1,server2,server3\n");
-		fprintf(conf, "continue=true\n");
-		fprintf(conf, "max-concurrent-downloads=1\n");
-		fprintf(conf, "max-connection-per-server=16\n");
-		fprintf(conf, "min-split-size=2M\n");
-		fprintf(conf, "split=16\n");;
-		fprintf(conf, "dir=Downloads/\n");
-		fclose(conf);
+	printf("\n正在更新tracker服务器. . .\n\n");
+	system("del best_aria2.txt");
+	if (system("aria2c -x16 https://trackerslist.com/best_aria2.txt") != 0) {
+		printf("\n更新失败，正在本地建立BT配置文件. . .\n");
+		if ((conf = fopen("bt.conf", "r")) == NULL) {
+			conf = fopen("bt.conf", "w");
+			fprintf(conf, "##bt-tracker=server1,server2,server3\n");
+			fprintf(conf, "continue=true\n");
+			fprintf(conf, "max-concurrent-downloads=1\n");
+			fprintf(conf, "max-connection-per-server=16\n");
+			fprintf(conf, "min-split-size=2M\n");
+			fprintf(conf, "split=16\n");;
+			fprintf(conf, "dir=Downloads/\n");
+			fprintf(conf, "user-agent=qBittorrent v4.2.5\n");
+			fprintf(conf, "peer-agent=qBittorrent v4.2.5\n");
+			fclose(conf);
+		}
+	}
+	else {
+		printf("\n更新成功，正在本地建立BT配置文件. . .\n");
+			conf = fopen("bt.conf", "w");
+			fprintf(conf, "bt-tracker=");
+			fclose(conf);
+			system("type best_aria2.txt>>bt.conf");
+			system("del best_aria2.txt");
+			conf = fopen("bt.conf", "a");
+			fprintf(conf, "\ncontinue=true\n");
+			fprintf(conf, "max-concurrent-downloads=1\n");
+			fprintf(conf, "max-connection-per-server=16\n");
+			fprintf(conf, "min-split-size=2M\n");
+			fprintf(conf, "split=16\n");;
+			fprintf(conf, "dir=Downloads/\n");
+			fprintf(conf, "user-agent=qBittorrent v4.2.5\n");
+			fprintf(conf, "peer-agent=qBittorrent v4.2.5\n");
+			fclose(conf);
 	}
 	printf("\n请在弹出窗口中修改BT配置文件,");
 	system("notepad bt.conf");
 	system("pause");
+	BroswerMark();
 	url();
 	return 0;
 }
@@ -245,15 +254,35 @@ int url() {
 			system("notepad.exe iqiyi.download");
 			sprintf(config_url, "%s", "-F iqiyi.download");
 		}
-		else {
-			if ((url = fopen("Pornhub.download", "r")) == NULL) {
-				url = fopen("Pornhub.download", "w");
+		else if (config_media == 5) {
+			if ((url = fopen("Douyu.download", "r")) == NULL) {
+				url = fopen("Douyu.download", "w");
 				fprintf(url, "%s", "## Input URL below (Don't delete this line)##\n");
 				fclose(url);
 			}
 			printf("\n请在弹出页输入下载地址. . .\n\n");
-			system("notepad.exe Pornhub.download");
-			sprintf(config_url, "%s", "-F Pornhub.download");
+			system("notepad.exe Douyu.download");
+			sprintf(config_url, "%s", "-F Douyu.download");
+		}
+		else if (config_media == 6) {
+			if ((url = fopen("Yinyuetai.download", "r")) == NULL) {
+				url = fopen("Yinyuetai.download", "w");
+				fprintf(url, "%s", "## Input URL below (Don't delete this line)##\n");
+				fclose(url);
+			}
+			printf("\n请在弹出页输入下载地址. . .\n\n");
+			system("notepad.exe Yinyuetai.download");
+			sprintf(config_url, "%s", "-F Yinyuetai.download");
+		}
+		else {
+			if ((url = fopen("NeteaseMusic.download", "r")) == NULL) {
+				url = fopen("NeteaseMusic.download", "w");
+				fprintf(url, "%s", "## Input URL below (Don't delete this line)##\n");
+				fclose(url);
+			}
+			printf("\n请在弹出页输入下载地址. . .\n\n");
+			system("notepad.exe NeteaseMusic.download");
+			sprintf(config_url, "%s", "-F NeteaseMusic.download");
 		}
 		}
 	else if (downloadmode == 4) {
@@ -298,22 +327,22 @@ int url() {
 int threader() {
 	int threader_fix;
 	if (downloadmode == 1 || downloadmode == 4) {
-		printf("\n同时下载任务数：");
-		scanf("%d", &Download_Task);//同时下载任务数
-			config_thread = 16;
-			sprintf(Downloader_Use, "%s", "aria2c.exe");
-			if (downloadmode != 2)sprintf(split, "1M");
-			else sprintf(split, "1M");
+		Download_Task = 1;//同时下载任务数
+		config_thread = 16;
+		sprintf(Downloader_Use, "%s", "aria2c.exe");
+		sprintf(split, "1M");
 	}
 	else if (downloadmode == 2) {
 		Download_Task = 1;//同时下载任务数
 		sprintf(Downloader_Use, "%s", "aria2c.exe");
-		if (mark == 1)config_thread = 5;
-		else if (mark == 2)config_thread = 5;
-		else if (mark == 3)config_thread = 3;
-		if (mark == 1)sprintf(split, "1M");
-		else if (mark == 2)sprintf(split, "1M");
-		else if (mark == 3)sprintf(split, "1M");
+		if (mark == 2) {
+			config_thread = 1;
+			sprintf(split, "1M");
+		}
+		else {
+			config_thread = 16;
+			sprintf(split, "2M");
+		}
 	}
 	else if (downloadmode == 3) {
 		Download_Task = 1;//同时下载任务数
@@ -395,67 +424,78 @@ int BroswerMark() {
 	}
 	else if(downloadmode==2){
 		printf("\n应用id请尝试250528或778750，下载失败请切换UA或应用id！\n");
-		printf("\n请选择浏览器标识：\n1.Wii版Opera浏览器（速度较快，不易出错）\n2.爱奇艺（速度最快，有的文件会报错）\n3.百度网盘客户端（兼容模式，速度慢）\n请输入：");
+		printf("\n请选择浏览器标识：\n1.爱奇艺（速度最快，有的文件会报错）\n2.百度网盘客户端（兼容模式，速度慢）\n请输入：");
 		scanf("%d", &mark);
 		if (mark == 1) {
-			sprintf(head, "--header=\"User-Agent:%s\"", "Opera/9.30 (Nintendo Wii; U; ; 2071; Wii Shop Channel/1.0; en)");//Wii游戏机版Opera
-			sprintf(head_show, "Wii游戏机版Opera（通用模式）");
-		}
-		else if (mark == 2) {
 			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Linux; Android 5.0; SM-N9100 Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36 NetType/WIFI Amoeba/1.0");//爱奇艺
 			sprintf(head_show, "%s", "爱奇艺（急速模式）");
 		}
-		else if (mark == 3) {
-			sprintf(head, "--header=\"User-Agent:%s\"", "netdisk;6.8.9.1;PC;PC-Windows;10.0.18363;WindowsBaiduYunGuanJia");//百度网盘
+		else if (mark == 2) {
+			sprintf(head, "--header=\"User-Agent:%s\"", "netdisk;6.9.7.4;PC;PC-Windows;10.0.18363;WindowsBaiduYunGuanJia");//百度网盘
 			sprintf(head_show, "%s", "百度网盘（兼容模式）");
 		}
 		else {
-			mark = 3;
-			sprintf(head, "--header=\"User-Agent:%s\"", "netdisk;6.8.9.1;PC;PC-Windows;10.0.18363;WindowsBaiduYunGuanJia");//百度网盘
+			mark = 2;
+			sprintf(head, "--header=\"User-Agent:%s\"", "netdisk;6.9.7.4;PC;PC-Windows;10.0.18363;WindowsBaiduYunGuanJia");//百度网盘
 			sprintf(head_show, "%s", "百度网盘（兼容模式）");
-		}
-		if (mark != 3) {
-			printf("\n若下载失败是否尝试切换用户代理（是=1，否=0）：");
-			scanf("%d", &Auto_ChangeUA);
-		}
-		else {
-			Auto_ChangeUA = 0;
 		}
 	}
-	else if(downloadmode==1 || downloadmode == 4||downloadmode==6){
-		printf("\n请选择浏览器标识：\n1.IE浏览器（增强模式）（普通下载推荐1）\n2.Windows版Opera浏览器\n3.Windows版Chrome（普通下载推荐2）\n4.Edge浏览器\n5.IE浏览器（兼容模式）\n6.自定义UserAgent\n请输入：");
+	else if(downloadmode==1){
+		printf("\n请选择浏览器标识：\n1.IE浏览器（增强模式）\n2.Windows版Chrome\n3.IE浏览器（兼容模式）\n4.自定义UserAgent\n请输入：");
 		scanf("%d", &mark);
 		if (mark == 1) {
 			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko");//IE浏览器（增强模式）
 			sprintf(head_show, "IE浏览器（增强模式）");
 		}
 		else if (mark == 2) {
-			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 6.0; Wow64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36 OPR/66.0.3515.36");//Windows版Opera
-			sprintf(head_show, "Windows版Opera");
-		}
-		else if (mark == 3) {
-			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36");//Windows版Chrome
+			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");//Windows版Chrome
 			sprintf(head_show, "Windows版Chrome");
 		}
-		else if (mark == 4) {
-			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363");//Edge浏览器
-			sprintf(head_show, "Edge浏览器");
-		}
-		else if (mark == 5) {
+		else if (mark == 3) {
 			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)");//IE浏览器（兼容模式）
 			sprintf(head_show, "IE浏览器（兼容模式）");
 		}
-		else if (mark == 6) {
+		else if (mark == 4) {
 			printf("\n请输入UserAgent的值：\n");
 			scanf("%s", UserAgent_DIY);
 			sprintf(head, "--header=\"User-Agent:%s\"", UserAgent_DIY);//自定义
 			sprintf(head_show, "自定义");
 		}
 		else {
-			mark = 1;
-			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 6.0; Trident/7.0; rv:11.0) like Gecko");//IE浏览器（增强模式）
+			mark = 2;
+			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");//IE浏览器（增强模式）
+			sprintf(head_show, "Windows版Chrome");
+		}
+	}
+	else if (downloadmode == 4) {
+		printf("\n请选择浏览器标识：\n1.IE浏览器（增强模式）\n2.Windows版Chrome\n3.IE浏览器（兼容模式）\n4.自定义UserAgent\n请输入：");
+		scanf("%d", &mark);
+		if (mark == 1) {
+			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko");//IE浏览器（增强模式）
 			sprintf(head_show, "IE浏览器（增强模式）");
 		}
+		else if (mark == 2) {
+			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");//Windows版Chrome
+			sprintf(head_show, "Windows版Chrome");
+		}
+		else if (mark == 3) {
+			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)");//IE浏览器（兼容模式）
+			sprintf(head_show, "IE浏览器（兼容模式）");
+		}
+		else if (mark == 4) {
+			printf("\n请输入UserAgent的值：\n");
+			scanf("%s", UserAgent_DIY);
+			sprintf(head, "--header=\"User-Agent:%s\"", UserAgent_DIY);//自定义
+			sprintf(head_show, "自定义");
+		}
+		else {
+			mark = 2;
+			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");//IE浏览器（增强模式）
+			sprintf(head_show, "Windows版Chrome");
+		}
+	}
+	else if (downloadmode == 6) {
+		sprintf(head_show, "qBittorrent v4.2.5");
 	}
 	return 0;
 }
@@ -470,19 +510,11 @@ int AdvanceDownloader() {
 	sprintf(reference, "--referer=\"%s\"", reference_input);
 	if ((cookie = fopen("Cookies.txt", "r")) == NULL) {
 	p_4:cookie = fopen("Cookies.txt", "w");
-		fprintf(cookie, "##请在浏览器登录并导出Cookie的值到本文件内\n");
-		fprintf(cookie, "##请将EditThisCookie插件设置中将导出格式设置为Netscape HTTP Cookie\n");
-		fprintf(cookie, "##Cookies的形式如下：\n");
-		fprintf(cookie, "##.jianshu.com	TRUE	/	TRUE	xxxx	_m7e_session_core	xxxxxxxxxxxxxxxxxxx\n");
-		fprintf(cookie, "##.jianshu.com	TRUE    /   FALSE	0	    xxxxxxxxxxxxxxxxx	xxxxxxx\n");
-		fprintf(cookie, "##导入完成记得保存\n\n");
+		fprintf(cookie, "# Input Cookie below#\n");
 		fclose(cookie);
 		printf("\n请在弹出窗口中导入Cookies信息，");
 		system("notepad Cookies.txt");
 		system("pause");
-		space = fopen("Cookies.txt", "a");
-		fprintf(space, "\n\n");
-		fclose(space);
 	}
 	else {
 		printf("\n检测到存在Cookies信息，是否继续使用上次的信息登录（是=1 否=0）：");
@@ -502,18 +534,13 @@ int Netdisk() {
 	int cookieuse;
 	FILE* cookie;
 	BroswerMark();
-	if(mark == 2)sprintf(reference, "%s", "--referer=\"http://pan.baidu.com/wap/home?#/?path=%2F&vmode=list\"");
+	if(mark == 1)sprintf(reference, "%s", "--referer=\"https://pan.baidu.com/wap/home#/\"");
 	else {
-		sprintf(reference, "%s", "--referer=\"http://pan.baidu.com/disk/home?#/all?path=%2F&vmode=list\"");
+		sprintf(reference, "%s", "--referer=\"https://pan.baidu.com/disk/home?#/all?path=%2F&vmode=list\"");
 	}
 	if ((cookie = fopen("Netdisk_Cookies.txt", "r")) == NULL) {
 	p_4:cookie = fopen("tmp.txt", "w");
-		fprintf(cookie, "##请在浏览器登录并导出百度网盘主页与pcs.baidu.com的所有Cookies的值到本文件内\n");
-		fprintf(cookie, "##请将EditThisCookie插件设置中将导出格式设置为Netscape HTTP Cookie\n");
-		fprintf(cookie, "##Cookies的形式如下：\n");
-		fprintf(cookie, "##.jianshu.com	TRUE	/	TRUE	xxxx	_m7e_session_core	xxxxxxxxxxxxxxxxxxx\n");
-		fprintf(cookie, "##.jianshu.com	TRUE    /   FALSE	0	    xxxxxxxxxxxxxxxxx	xxxxxxx\n");
-		fprintf(cookie, "##导入完成记得保存\n\n");
+		fprintf(cookie, "# Input Cookie below#\n");
 		fclose(cookie);
 		printf("\n请在弹出窗口中导入百度网盘Cookies信息，");
 		system("notepad tmp.txt");
@@ -521,9 +548,6 @@ int Netdisk() {
 		system("type tmp.txt | find \"BDUSS\">Netdisk_Cookies.txt");
 		system("type tmp.txt | find \"pcsett\">>Netdisk_Cookies.txt");
 		system("del tmp.txt");
-		space = fopen("Netdisk_Cookies.txt", "a");
-		fprintf(space, "\n\n");
-		fclose(space);
 	}
 	else {
 		printf("\n检测到存在Cookies信息，是否继续使用上次的信息登录（是=1 否=0）：");
@@ -574,12 +598,12 @@ int AutoShutdown(int mode) {
 }
 
 int MediaDownloader() {
-	FILE* Bilibili_Cookies,*ytb_Cookies,* QQVideo_Cookies,*iqiyi_Cookies,*Pornhub_Cookies;
+	FILE* Bilibili_Cookies,*ytb_Cookies,* QQVideo_Cookies,*iqiyi_Cookies,*Douyu_Cookies,*NeteaseMusic_Cookies,*Yinyuetai_Cookies;
 	int StartAt,EndAt;
-	printf("\n下载视频来源：\n\n1.Youtube\n\n2.B站（兼容番剧下载）\n\n3.腾讯视频\n\n4.爱奇艺\n\n5.Pornhub\n\n请输入：");
+	printf("\n下载音视频来源：\n\n1.油管\n\n2.哔哩哔哩\n\n3.腾讯视频\n\n4.爱奇艺\n\n5.斗鱼\n\n6.音悦台\n\n7.网易云音乐\n\n请输入：");
 	scanf("%d", &config_media);
 	if (config_media == 1) {
-		printf("\n是否下载整个列表内所有视频（是=1，否=0）：");
+		printf("\n是否下载整个列表内所有音视频（是=1，否=0）：");
 		scanf("%d", &use_list);
 		if (use_list == 0) {
 			sprintf(play_list, "--no-playlist");
@@ -589,7 +613,7 @@ int MediaDownloader() {
 		}
 	}
 	else {
-		printf("\n是否下载整个列表内所有视频（是=1，只下载当前视频=0，选择集数=3）：");
+		printf("\n是否下载整个列表内所有音视频（是=1，只下载当前视频=0，选择集数=3）：");
 		scanf("%d", &use_list);
 		if (use_list == 0) {
 			sprintf(play_list, "");
@@ -639,13 +663,31 @@ int MediaDownloader() {
 		system("notepad iqiyi_Cookies.txt");
 		printf("\n请在弹出窗口中导入Cookies，同一账号可以反复使用\n\n");
 	}
-	else{
-		if ((Pornhub_Cookies = fopen("Pornhub_Cookies.txt", "r")) == NULL) {
-			Pornhub_Cookies = fopen("Pornhub_Cookies.txt", "w");
-			fprintf(Pornhub_Cookies, "# Input Cookie below#\n");
-			fclose(Pornhub_Cookies);
+	else if (config_media == 5) {
+		if ((Douyu_Cookies = fopen("Douyu_Cookies.txt", "r")) == NULL) {
+			Douyu_Cookies = fopen("Douyu_Cookies.txt", "w");
+			fprintf(Douyu_Cookies, "# Input Cookie below#\n");
+			fclose(Douyu_Cookies);
 		}
-		system("notepad Pornhub_Cookies.txt");
+		system("notepad Douyu_Cookies.txt");
+		printf("\n请在弹出窗口中导入Cookies，同一账号可以反复使用\n\n");
+	}
+	else if (config_media == 6) {
+		if ((Yinyuetai_Cookies = fopen("Yinyuetai_Cookies.txt", "r")) == NULL) {
+			Yinyuetai_Cookies = fopen("Yinyuetai_Cookies.txt", "w");
+			fprintf(Yinyuetai_Cookies, "# Input Cookie below#\n");
+			fclose(Yinyuetai_Cookies);
+		}
+		system("notepad Yinyuetai_Cookies.txt");
+		printf("\n请在弹出窗口中导入Cookies，同一账号可以反复使用\n\n");
+	}
+	else {
+		if ((NeteaseMusic_Cookies = fopen("NeteaseMusic_Cookies.txt", "r")) == NULL) {
+			NeteaseMusic_Cookies = fopen("NeteaseMusic_Cookies.txt", "w");
+			fprintf(NeteaseMusic_Cookies, "# Input Cookie below#\n");
+			fclose(NeteaseMusic_Cookies);
+		}
+		system("notepad NeteaseMusic_Cookies.txt");
 		printf("\n请在弹出窗口中导入Cookies，同一账号可以反复使用\n\n");
 	}
 	url();
@@ -656,7 +698,7 @@ int MediaDownloader() {
 }
 
 int downloadengine() {
-	FILE* Bilibili_Download,*ytb_Download,*QQVideo_Download,*iqiyi_Download,*Pornhub_Download;
+	FILE* Bilibili_Download,*ytb_Download,*QQVideo_Download,*iqiyi_Download,*Douyu_Download,*Yinyuetai_Download,*NeteaseMusic_Download;
 	int download_result1;
 	if (downloadmode == 1) {
 		sprintf(cmd, "%s -c -x%d -k%s -j %d %s %s %s %s", Downloader_Use, config_thread, split, Download_Task, config_dir, config_proxy, head, config_url);
@@ -692,12 +734,23 @@ int downloadengine() {
 			fclose(iqiyi_Download);
 
 		}
+		else if (config_media == 5) {
+			Douyu_Download = fopen("Douyu_Download.bat", "w");
+			fprintf(Douyu_Download, "%s -c Douyu_Cookies.txt %s %s -aria2 %s\n", Downloader_Use, play_list, config_dir, config_url);
+			fprintf(Douyu_Download, "exit\n");
+			fclose(Douyu_Download);
+		}
+		else if (config_media == 6) {
+			Yinyuetai_Download = fopen("Yinyuetai_Download.bat", "w");
+			fprintf(Yinyuetai_Download, "%s -c Yinyuetai_Cookies.txt %s %s -aria2 %s\n", Downloader_Use, play_list, config_dir, config_url);
+			fprintf(Yinyuetai_Download, "exit\n");
+			fclose(Yinyuetai_Download);
+		}
 		else {
-			Pornhub_Download = fopen("Pornhub_Download.bat", "w");
-			fprintf(Pornhub_Download, "%s -c Pornhub_Cookies.txt %s %s -aria2 %s\n", Downloader_Use, play_list, config_dir, config_url);
-			fprintf(Pornhub_Download, "exit\n");
-			fclose(Pornhub_Download);
-
+			NeteaseMusic_Download = fopen("NeteaseMusic_Download.bat", "w");
+			fprintf(NeteaseMusic_Download, "%s -c NeteaseMusic_Cookies.txt %s %s -aria2 %s\n", Downloader_Use, play_list, config_dir, config_url);
+			fprintf(NeteaseMusic_Download, "exit\n");
+			fclose(NeteaseMusic_Download);
 		}
 	}
 	else if (downloadmode == 4) {
@@ -746,8 +799,14 @@ int downloadengine() {
 		else if (config_media == 4) {
 			system("start "" /min iqiyi_Download.bat");
 		}
+		else if (config_media == 5) {
+			system("start "" /min Douyu_Download.bat");
+		}
+		else if (config_media == 6) {
+			system("start "" /min Yinyuetai_Download.bat");
+		}
 		else {
-			system("start "" /min Pornhub_Download.bat");
+			system("start "" /min NeteaseMusic_Download.bat");
 		}
 			printf("\n由于系统限制，下载进程无法自动停止，若弹出窗口一直显示complete，或需停止下载，");
 			system("pause");
