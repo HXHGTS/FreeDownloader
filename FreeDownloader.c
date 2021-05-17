f59129afb6d4535cb52ae082b942a13672eaf1ad
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-05-18 02:29:29 +0800

    Add files via upload

--- FreeDownloader.c
+++ FreeDownloader.c
@@ -7,11 +7,11 @@ int AdvanceDownloader(),AutoShutdown(int mode),BroswerMark(),CheckSum(int mode),
 int MagnetDownloader(),MediaDownloader(),Netdisk(),NormalDownloader(),proxyswitcher(),threader(),url();
 int downloadmode, magnet_mode,ConnectionNum, ProcessNum, config_media, anti_shutdown, Task, IsCheckSum;
 int mark, redownload_result, shutdown, filecheck, DownloadList, OpenDir;
-int cookie_import, cookie_mode,appid;
-int scan_return;//接收返回值，暂时没用
-char config_proxy[65], config_url[260], config_dir[35], config_cookie[230], smallcmd[20], Downloader_Use[12];
+int cookie_import, cookie_mode,appid,Output_Choose;
+int scan_return;//接收返回值,暂时没用
+char config_proxy[65], config_url[260], config_dir[35], config_cookie[230], smallcmd[20], Downloader_Use[20],Output_Format[5];
 char reference[216], head[300], head_show[35];
-char location[200],split[7],torrent_loca[250],play_list[40], color[4];
+char location[200],split[7],torrent_addr[250],play_list[40], color[4];
 char proxy[50];
 char rpctoken[40];//定义rpc密钥
 char BDUSS[193];
@@ -21,7 +21,7 @@ FILE* cookie,*bat,*dht;
 
 int TokenGenerate() {
 	if ((_access("config\\uuid", 0))==-1) {
-		system("powershell [guid]::NewGuid() | find /v \"Guid\" | find /v \"--\" | find \"-\" > config\\uuid");//随机GUID作为密码，提高RPC功能安全性
+		system("powershell [guid]::NewGuid() | find /v \"Guid\" | find /v \"--\" | find \"-\" > config\\uuid");//随机GUID作为密码,提高RPC功能安全性
 	}
 	conf = fopen("config\\uuid", "r");
 	scan_return=fscanf(conf,"%s", rpctoken);
@@ -32,7 +32,7 @@ int TokenGenerate() {
 int WindowSkin() {
 	if ((_access("config\\skin.ini", 0))==-1) {
 		printf("重置窗体与字体颜色请直接删除config目录下skin.ini文件！\n");
-		printf("请按照\"窗口颜色+字体颜色\"的格式设置皮肤，对应关系如下：\n");
+		printf("请按照\"窗口颜色+字体颜色\"的格式设置皮肤,对应关系如下:\n");
 		printf("------------------------------------------------------------------\n");
 		printf("0=黑色   8=灰色\n");
 		printf("1=蓝色   9=淡蓝色\n");
@@ -43,7 +43,7 @@ int WindowSkin() {
 		printf("6=黄色   E=淡黄色\n");
 		printf("7=白色   F=亮白色\n");
 		printf("------------------------------------------------------------------\n");
-		printf("请输入以配置界面与字体颜色，如0A：");
+		printf("请输入以配置界面与字体颜色,如0A:");
 		scan_return=scanf("%s", color);
 		skin = fopen("config\\skin.ini", "w");
 		fprintf(skin,"%s",color);
@@ -67,7 +67,7 @@ int CreateConfig() {
 	}
 	if ((_access("config\\dht.dat", 0))==-1) {
 		system("curl https://hxhgts.ml/FreeDownloader/dht.dat -# > config\\dht.dat");
-		system("dht链路优化完成，重启软件生效!\n");
+		system("dht链路优化完成,重启软件生效!\n");
 		exit(0);
 	}
 	printf("\n正在尝试连接到trackerslist.com服务器. . .\n\n");
@@ -76,11 +76,11 @@ int CreateConfig() {
 	fclose(conf);
 	sprintf(cmd, "curl https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection/best_aria2.txt -# > config\\best_aria2.txt");
 	if (system(cmd) != 0) {
-		printf("\n更新失败，正在本地建立BT配置文件. . .\n");
+		printf("\n更新失败,正在本地建立BT配置文件. . .\n");
 		system("notepad config\\bt.conf");
 	}
 	else {
-		printf("\n更新成功，正在本地建立BT配置文件. . .\n");
+		printf("\n更新成功,正在本地建立BT配置文件. . .\n");
 		system("type config\\best_aria2.txt >> config\\bt.conf");
 		system("del /F /S /Q config\\best_aria2.txt");
 	}
@@ -132,10 +132,10 @@ int preload() {
 	WindowSkin();
 	sprintf(smallcmd, "color %s", color);
 	system(smallcmd);
-	printf("需要系统UAC权限读取代理服务器数据，若需要使用代理服务器请在打开本软件前打开代理. . .\n\n");
+	printf("需要系统UAC权限读取代理服务器数据,若需要使用代理服务器请在打开本软件前打开代理. . .\n\n");
 	system("Timeout /T 2");
-	if (system("start /min GetProxyInfo.exe") != 0) {
-		printf("UAC授权失败，无法检测计算机代理设置！\n\n");
+	if (system("start /min bin\\GetProxyInfo.exe") != 0) {
+		printf("UAC授权失败,无法检测计算机代理设置！\n\n");
 		conf = fopen("config\\proxy.ini", "w");
 		fprintf(conf, "proxy=0");
 		fclose(conf);
@@ -149,13 +149,13 @@ MainMenu:system(smallcmd);
 	printf("------------------------------------------------------------------\n");
 	printf("--------------------------FreeDownloader--------------------------\n");
 	printf("------------------------------------------------------------------\n");
-	printf("请选择下载功能：\n1.普通下载模式\n2.百度网盘模式\n3.视频下载模式\n4.高级下载模式\n5.磁力下载模式\n6.文件完整性测试\n7.Github上的软件帮助\n8.启动RPC被动监听\n9.打开下载文件夹\n0.退出\n");
+	printf("请选择下载功能:\n1.普通下载模式\n2.百度网盘模式\n3.视频下载模式\n4.高级下载模式\n5.磁力下载模式\n6.文件完整性测试\n7.Github上的软件帮助\n8.启动RPC被动监听\n9.打开下载文件夹\n0.退出\n");
 	printf("------------------------------------------------------------------\n");
-	printf("请输入：");
+	printf("请输入:");
 	scan_return=scanf("%d", &downloadmode);
 	if (system("type config\\power.ini | find \"power=1\"") == 0) {
 		shutdown = 1;
-		system("echo \"power=0\" config\\power.ini"); //设置后只生效一次，自动还原状态，避免每次都自动关机
+		system("echo \"power=0\" config\\power.ini"); //设置后只生效一次,自动还原状态,避免每次都自动关机
 	}
 	else {
 		shutdown = 0;
@@ -214,7 +214,7 @@ Downloading:redownload_result = downloadengine();
 		printf("-----------------------------下载成功!----------------------------\n");
 		printf("------------------------------------------------------------------\n");
 		AutoShutdown(shutdown);
-		printf("\n是否打开下载文件夹：\n\n1.是\n\n0.否\n\n请输入：");
+		printf("\n是否打开下载文件夹:\n\n1.是\n\n0.否\n\n请输入:");
 		scan_return=scanf("%d", &OpenDir);
 		if (OpenDir != 0) {
 			system("explorer Downloads");
@@ -284,9 +284,9 @@ int ListenRPC() {
 int MagnetDownloader() {
 	CreateConfig();
 	printf("----------------------------------------------------------------------------------\n");
-	printf("注意:本模式下代理在下载阶段不起作用，若需要使用代理请配合Tap/Tun工具强制接管流量！\n");
+	printf("注意:本模式下代理在下载阶段不起作用,若需要使用代理请配合Tap/Tun工具强制接管流量！\n");
 	printf("----------------------------------------------------------------------------------\n");
-	printf("请选择下载模式：\n\n1.*.torrent文件\n\n2.Magnet://链接\n\n0.返回\n\n请输入：");
+	printf("请选择下载模式:\n\n1.*.torrent文件\n\n2.Magnet://链接\n\n0.返回\n\n请输入:");
 	scan_return=scanf("%d", &magnet_mode);
 	if (magnet_mode == 2) {
 		dir();
@@ -307,7 +307,8 @@ int url() {
 			url = fopen("temp\\normal.download", "w");
 			fclose(url);
 		}
-		printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+		system("cls");
+		printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 		system("notepad.exe temp\\normal.download");
 		sprintf(config_url, "%s", "-i temp\\normal.download");
 	}
@@ -316,7 +317,8 @@ int url() {
 					url = fopen("temp\\netdisk.download", "w");
 					fclose(url);
 		}
-		printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+		system("cls");
+		printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 		system("notepad.exe temp\\netdisk.download");
 		sprintf(config_url, "%s", "-i temp\\netdisk.download");
 		system("del temp\\url.bat");
@@ -327,7 +329,8 @@ int url() {
 				url = fopen("temp\\ytb.download", "w");
 				fclose(url);
 			}
-			printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+			system("cls");
+			printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 			system("notepad.exe temp\\ytb.download");
 			sprintf(config_url, "%s", "-a temp\\ytb.download");
 		}
@@ -337,7 +340,8 @@ int url() {
 				fprintf(url, "---Input URL(Don't delete this line)---\n");
 				fclose(url);
 			}
-			printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+			system("cls");
+			printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 			system("notepad.exe temp\\Bilibili.download");
 			sprintf(config_url, "%s", "-F temp\\Bilibili.download");
 		}
@@ -347,7 +351,8 @@ int url() {
 				fprintf(url, "---Input URL(Don't delete this line)---\n");
 				fclose(url);
 			}
-			printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+			system("cls");
+			printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 			system("notepad.exe temp\\QQVideo.download.1");
 			sprintf(config_url, "%s", "-F temp\\QQVideo.download");
 		}
@@ -357,7 +362,8 @@ int url() {
 				fprintf(url, "---Input URL(Don't delete this line)---\n");
 				fclose(url);
 			}
-			printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+			system("cls");
+			printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 			system("notepad.exe temp\\iqiyi.download");
 			sprintf(config_url, "%s", "-F temp\\iqiyi.download");
 		}
@@ -367,7 +373,8 @@ int url() {
 				fprintf(url, "---Input URL(Don't delete this line)---\n");
 				fclose(url);
 			}
-			printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+			system("cls");
+			printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 			system("notepad.exe temp\\Youku.download");
 			sprintf(config_url, "%s", "-F temp\\Youku.download");
 		}
@@ -377,7 +384,8 @@ int url() {
 			url = fopen("temp\\advance.download", "w");
 			fclose(url);
 		}
-		printf("\n请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+		system("cls");
+		printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 		system("notepad.exe temp\\advance.download");
 		sprintf(config_url, "%s", "-i temp\\advance.download");
 	}
@@ -388,15 +396,17 @@ int url() {
 				fprintf(url, "magnet:?xt=urn:btih:");
 				fclose(url);
 			}
-			printf("请在弹出页输入下载地址，多个地址可以用回车隔开. . .\n\n");
+			system("cls");
+			printf("请在弹出页输入下载地址,多个地址可以用回车隔开. . .\n\n");
 			system("notepad.exe temp\\magnet.download");
 			sprintf(config_url, "%s", "-i temp\\magnet.download");
 		}
 		else {
-			printf("若种子文件名过长建议重命名成简单字母或数字再拖入窗口，否则可能报错！\n\n");
-			printf("请将种子文件以拖拽至本窗口中：");
-			scan_return=scanf("%s", torrent_loca);
-			sprintf(config_url, "\"%s\"",torrent_loca);
+			system("cls");
+			printf("若种子文件名过长建议重命名成简单字母或数字再拖入窗口,否则可能报错！\n\n");
+			printf("请将种子文件以拖拽至本窗口中:");
+			scan_return=scanf("%s", torrent_addr);
+			sprintf(config_url, "\"%s\"",torrent_addr);
 		}
 	}
 	return 0;
@@ -407,29 +417,29 @@ int threader() {
 		Task = 1;//同时下载任务数
 		ConnectionNum = 16;
 		ProcessNum = 4;
-		sprintf(Downloader_Use, "%s", "aria2c");
+		sprintf(Downloader_Use, "%s", "bin\\aria2c");
 		sprintf(split, "1M");
 	}
 	else if (downloadmode == 2) {
 		ConnectionNum = 6;
 		ProcessNum = 6;
 		sprintf(split, "1M");
-		sprintf(Downloader_Use, "%s", "aria2c");
+		sprintf(Downloader_Use, "%s", "bin\\aria2c");
 		Task = 1;//同时下载任务数
 	}
 	else if (downloadmode == 3) {
 		Task = 1;//同时下载任务数
 		if (config_media == 1) {
-			sprintf(Downloader_Use, "%s", "youtube-dl");
+			sprintf(Downloader_Use, "%s", "bin\\youtube-dl");
 		}
 		else {
-			sprintf(Downloader_Use, "%s", "annie");
+			sprintf(Downloader_Use, "%s", "bin\\annie");
 		}
 		ConnectionNum = 1;//4k视频状态下aria2调用出现bug
 	}
 	else if (downloadmode == 5) {
 		Task = 1;//同时下载任务数
-		sprintf(Downloader_Use, "%s", "aria2c");
+		sprintf(Downloader_Use, "%s", "bin\\aria2c");
 		ConnectionNum = 16;
 		if (magnet_mode == 2)sprintf(split, "1M");
 	}
@@ -477,7 +487,7 @@ int proxyswitcher() {
 			
 	}
 	system("cls");
-	return 0;// /config/proxy.ini中proxy=0或此文件不存在为无代理状态，否则使用代理(仅支持http/https代理)
+	return 0;// /config/proxy.ini中proxy=0或此文件不存在为无代理状态,否则使用代理(仅支持http/https代理)
 }
 
 int BroswerMark() {
@@ -494,7 +504,7 @@ int BroswerMark() {
 		sprintf(head_show, "Chrome");
 	}
 	else if (downloadmode == 4) {
-		printf("\n请选择浏览器标识：\n\n1.IE\n\n2.Chrome\n\n3.Chrome(Mobile)\n\n请输入：");
+		printf("\n请选择浏览器标识:\n\n1.IE\n\n2.Chrome\n\n3.Chrome(Mobile)\n\n请输入:");
 		scan_return=scanf("%d", &mark);
 		if (mark == 1) {
 			sprintf(head, "--header=\"User-Agent:%s\"", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)");//IE浏览器
@@ -515,7 +525,7 @@ int BroswerMark() {
 		}
 	}
 	else if (downloadmode == 5) {
-		printf("\n请选择BT下载工具标识：\n\n1.qBittorrent\n\n2.Transmission\n\n3.uTorrent\n\n4.BitComet\n\n请输入：");
+		printf("\n请选择BT下载工具标识:\n\n1.qBittorrent\n\n2.Transmission\n\n3.uTorrent\n\n4.BitComet\n\n请输入:");
 		scan_return = scanf("%d", &mark);
 		if (mark == 1) {
 			sprintf(head, "user-agent=%s\npeer-agent=%s\npeer-id-prefix=%s", "qBittorrent/4.3.5.0", "qBittorrent/4.3.5.0", "-qB4350-");//qBittorrent
@@ -547,19 +557,19 @@ int AdvanceDownloader() {
 	char reference_input[200];
 	FILE* cookie;
 	BroswerMark();
-	printf("\n请输入下载引用页地址：\n");
+	printf("\n请输入下载引用页地址:\n");
 	scan_return=scanf("%s", reference_input);
 	sprintf(reference, "--referer=\"%s\"", reference_input);
 	if (fopen("cookies\\Cookies.txt", "r") == NULL) {
 	p_4:cookie = fopen("cookies\\Cookies.txt", "w");
 		fprintf(cookie, "# Input Cookie below#\n");
 		fclose(cookie);
-		printf("\n请在弹出窗口中导入Cookies信息，");
+		printf("\n请在弹出窗口中导入Cookies信息,");
 		system("notepad cookies\\Cookies.txt");
 		system("pause");
 	}
 	else {
-		printf("\n检测到存在Cookies信息，是否继续使用上次的信息登录（是=1 否=0）：");
+		printf("\n检测到存在Cookies信息,是否继续使用上次的信息登录（是=1 否=0）:");
 		scan_return=scanf("%d", &cookie_import);
 		if (cookie_import != 1) {
 			goto p_4;
@@ -589,7 +599,7 @@ int Netdisk() {
 				system("del temp\\Netdisk_Cookies_tmp.txt");
 			}
 			else {
-				printf("\n检测到存在Cookies信息，是否继续使用上次的信息登录（是=1 否=0）：");
+				printf("\n检测到存在Cookies信息,是否继续使用上次的信息登录（是=1 否=0）:");
 				scan_return=scanf("%d", &cookie_import);
 				if (cookie_import != 1) {
 					goto p_4;
@@ -606,7 +616,7 @@ int Netdisk() {
 				fclose(cookie);
 			}
 			else {
-				printf("\n检测到存在Cookies信息，是否继续使用上次的信息登录（是=1 否=0）：");
+				printf("\n检测到存在Cookies信息,是否继续使用上次的信息登录（是=1 否=0）:");
 				scan_return = scanf("%d", &cookie_import);
 				if (cookie_import != 1) {
 					goto p_5;
@@ -626,8 +636,8 @@ int Netdisk() {
 
 int CheckSum(int mode) {
 	if (mode == 1) {
-		printf("\n如果需要检测文件完整性，可以将文件拖拽至本窗口内！建议先将文件名称最简化并移至磁盘根目录下再检测！\n");
-		printf("\n文件名称中不允许空格！若md5不一致，则文件损坏，需重新下载！\n\n文件路径：");
+		printf("\n如果需要检测文件完整性,可以将文件拖拽至本窗口内！建议先将文件名称最简化并移至磁盘根目录下再检测！\n");
+		printf("\n文件名称中不允许空格！若md5不一致,则文件损坏,需重新下载！\n\n文件路径:");
 		scan_return=scanf("%s", location);
 		sprintf(cmd, "CertUtil -hashfile \"%s\" MD5", location);
 		printf("\n\n正在计算MD5值. . .\n");
@@ -657,7 +667,7 @@ int CheckSum(int mode) {
 int AutoShutdown(int mode) {
 	if (mode == 1) {
 		system("shutdown -s -t 60");
-		printf("是否阻止系统关机(是=1)：");
+		printf("是否阻止系统关机(是=1):");
 		scan_return=scanf("%d", &anti_shutdown);
 		if (anti_shutdown == 1) {
 			system("shutdown -a");
@@ -669,11 +679,11 @@ int AutoShutdown(int mode) {
 int MediaDownloader() {
 	FILE* Bilibili_Cookies,*ytb_Cookies,* QQVideo_Cookies,*iqiyi_Cookies,*Youku_Cookies;
 	char chapter[14];
-	printf("下载音视频来源：\n\n1.油管\n\n2.哔哩哔哩\n\n3.腾讯视频\n\n4.爱奇艺\n\n5.优酷\n\n0.返回\n\n请输入：");
+	printf("下载音视频来源:\n\n1.油管\n\n2.哔哩哔哩\n\n3.腾讯视频\n\n4.爱奇艺\n\n5.优酷\n\n0.返回\n\n请输入:");
 	scan_return=scanf("%d", &config_media);
 	proxyswitcher();
-	if (config_media != 1&&config_media!=0) {
-		printf("\n下载整个列表内所有音视频？\n\n1.是\n\n2.只下载当前视频\n\n请输入：");
+	if (config_media != 1&&config_media!=0) {//调用annie
+		printf("\n下载整个列表内所有音视频?\n\n1.是\n\n2.只下载当前视频\n\n请输入:");
 		scan_return = scanf("%d", &DownloadList);
 		if (DownloadList == 1) {
 			sprintf(play_list, "-p");
@@ -682,8 +692,8 @@ int MediaDownloader() {
 			sprintf(play_list, "");
 		}
 	}
-	else if(config_media!=0){
-		printf("\n下载整个列表内所有音视频？\n\n1.是\n\n2.只下载当前视频\n\n0.选择集数\n\n请输入：");
+	else if(config_media!=0){//调用youtube-dl
+		printf("\n下载整个列表内所有音视频?\n\n1.是\n\n2.只下载当前视频\n\n0.选择集数\n\n请输入:");
 		scan_return = scanf("%d", &DownloadList);
 		if (DownloadList == 1) {
 			sprintf(play_list, "--yes-playlist");
@@ -692,9 +702,19 @@ int MediaDownloader() {
 			sprintf(play_list, "--no-playlist");
 		}
 		else {
-			printf("\n请按照格式输入下载范围，如1-5,6,7,8-15：");
+			system("cls");
+			printf("请按照格式输入下载范围,如1-5,6,7,8-15:");
 			scan_return = scanf("%s", chapter);
 			sprintf(play_list, "--playlist-items %s", chapter);
+		}
+		system("cls");
+		printf("请选择导出格式:\n\n1.mkv\n\n2.mp4\n\n请输入:");
+		scan_return = scanf("%d", &Output_Choose);
+		if (Output_Choose == 1) {
+			sprintf(Output_Format, "mkv");
+		}
+		else {
+			sprintf(Output_Format, "mp4");
 		}
 	}
 	else {
@@ -740,7 +760,8 @@ int MediaDownloader() {
 			}
 			system("notepad cookies\\Youku_Cookies.txt");
 	}
-	printf("\n请在弹出窗口中导入Cookies，同一账号可以反复使用\n\n");
+	system("cls");
+	printf("请在弹出窗口中导入Cookies,同一账号可以反复使用\n\n");
 	url();
 	threader();
 	dir();
@@ -769,7 +790,7 @@ int downloadengine() {
 			ytb_Download = fopen("temp\\ytb_Download.bat", "w");
 			fprintf(ytb_Download, "@echo off\n");
 			fprintf(ytb_Download, "%s\n", config_proxy);
-			fprintf(ytb_Download, "%s --cookies cookies\\ytb_Cookies.txt --write-sub --all-subs %s %s %s --external-downloader aria2c --external-downloader-args \"-x16 -k2M --file-allocation=none\"\n", Downloader_Use, play_list, config_dir, config_url);
+			fprintf(ytb_Download, "%s --cookies cookies\\ytb_Cookies.txt --write-sub --all-subs %s %s --merge-output-format %s %s --external-downloader aria2c --external-downloader-args \"-x16 -k2M --file-allocation=none\"\n", Downloader_Use, play_list, config_dir, Output_Format,config_url);
 			fclose(ytb_Download);
 		}
 		else if (config_media == 2) {
@@ -820,13 +841,16 @@ int downloadengine() {
 	}
 	system("cls");
 	printf("------------------------------------------------------------------\n");
-	printf("代理地址：%s(为空代表没有设置代理)\n", config_proxy);
-	printf("下载线程数：%d\n", ConnectionNum);
-	printf("下载引擎：%s\n", Downloader_Use);
-	printf("浏览器标识：%s\n", head_show);
+	printf("代理地址:%s(为空代表没有设置代理)\n", config_proxy);
+	printf("下载线程数:%d\n", ConnectionNum);
+	printf("下载引擎:%s\n", Downloader_Use);
+	printf("浏览器标识:%s\n", head_show);
+	if (downloadmode == 3 && config_media == 1) {
+		printf("导出格式:*.%s\n", Output_Format);
+	}
 	printf("------------------------------------------------------------------\n");
-	printf("下载过程中出现的红色ERROR报错可忽略，对下载没有影响！\n");
-	printf("下载正在执行，希望中断下载建议按Ctrl+C以正常退出. . .\n");
+	printf("下载过程中出现的红色ERROR报错可忽略,对下载没有影响！\n");
+	printf("下载正在执行,希望中断下载建议按Ctrl+C以正常退出. . .\n");
 	printf("------------------------------------------------------------------\n");
 	if (downloadmode == 3) {
 		if (config_media == 1) {
