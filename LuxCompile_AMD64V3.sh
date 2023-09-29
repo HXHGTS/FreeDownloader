e1088d187d192396f25b9e7aa6dce3c4e15eda5e
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-09-29 18:10:16 +0800

    Create LuxCompile_AMD64V3.sh

---
+++ LuxCompile_AMD64V3.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo '正在安装/升级必需插件. . .'
+
+apt update
+
+apt install -y curl wget tar gawk sed
+
+echo '正在安装/升级go. . .'
+
+apt remove -y --purge golang
+
+apt autoremove -y
+
+rm -rf /usr/local/go
+
+Go_Version=$(curl https://github.com/golang/go/tags | grep '/releases/tag/go' | head -n 1 | gawk -F/ '{print $6}' | gawk -F\" '{print $1}')
+
+wget -O /var/tmp/${Go_Version}.linux-amd64.tar.gz https://go.dev/dl/${Go_Version}.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf /var/tmp/${Go_Version}.linux-amd64.tar.gz
+
+rm -f /var/tmp/${Go_Version}.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+go version
+
+echo '正在编译Windows版lux核心. . .'
+
+env GOOS=windows GOARCH=amd64 GOAMD64=v3 CGO_ENABLED=0 go install github.com/iawia002/lux@latest
+
+echo 'lux核心已编译!'
+
+exit 0
