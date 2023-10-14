043ea534a54860e647608678c0ddf468bdb78109
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-10-15 00:22:27 +0800

    Update LuxCompile_AMD64V3.sh

--- LuxCompile_AMD64V3.sh
+++ LuxCompile_AMD64V3.sh
@@ -16,6 +16,8 @@ rm -rf /usr/local/go
 
 Go_Version=$(curl https://github.com/golang/go/tags | grep '/releases/tag/go' | head -n 1 | gawk -F/ '{print $6}' | gawk -F\" '{print $1}')
 
+#Go_Version=$(curl https://go.dev/dl/ | grep download | gawk -F'>' '{print $3}' | gawk -F'<' '{print $1}' | grep linux-amd64 | head -n 1 | sed -e "s/go//g" | sed -e "s@go@.linux-amd64.tar.gz@g")
+
 wget -O /var/tmp/${Go_Version}.linux-amd64.tar.gz https://go.dev/dl/${Go_Version}.linux-amd64.tar.gz
 
 tar -C /usr/local -xzf /var/tmp/${Go_Version}.linux-amd64.tar.gz
